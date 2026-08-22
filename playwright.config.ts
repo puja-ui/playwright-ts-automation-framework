c636c22cb020a572bceb83d81b5c50ad07239bad
Puja Karmakar <pujakarmakar2k@gmail.com>
2026-08-22 16:20:11 +0530

    feat: integrate qase reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,9 +5,12 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -23,7 +26,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['playwright-qase-reporter', {
+      apiToken: process.env.QASE_API_TOKEN,
+      projectCode: process.env.QASE_PROJECT_CODE,
+      runComplete: true,
+      logging: true,
+      uploadAttachments: true,
+    }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
